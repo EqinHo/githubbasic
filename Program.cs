4528fd7872979cd6bc5da58437ba442cf704d0ea
EqinHo <Quyen.Ho@gokstadakademiet.no>
2023-01-31 18:35:59 +0100

    kun en linje kode igjen

--- Program.cs
+++ Program.cs
@@ -3,17 +3,6 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!dette er en test");
-        Console.WriteLine("Hello, World!dette er en test");
-        Console.WriteLine("Hello, World!dette er en test");
-        Console.WriteLine("Hello, World!dette er en test"); Console.WriteLine("Hello, World!dette er en test");
-        Console.WriteLine("Hello, World!dette er en test"); Console.WriteLine("Hello, World!dette er en test");
-        Console.WriteLine("Hello, World!dette er en test"); Console.WriteLine("Hello, World!dette er en test");
-        Console.WriteLine("Hello, World!dette er en test"); Console.WriteLine("Hello, World!dette er en test");
-        Console.WriteLine("Hello, World!dette er en test"); Console.WriteLine("Hello, World!dette er en test");
-        Console.WriteLine("Hello, World!dette er en test");
-
-
-
 
     }
 
